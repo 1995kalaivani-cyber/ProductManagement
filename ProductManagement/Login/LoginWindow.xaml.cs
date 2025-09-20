6d4f8b96c2cc65e839c971bad243b22c17c83b5f
1995kalaivani-cyber <1995kalaivani@gmail.com>
2025-09-20 19:35:47 +0530

    new project creater

---
+++ ProductManagement/Login/LoginWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProductManagement.Login
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnlogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnreg_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ProductManagement/Login/Register.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProductManagement.Login
+{
+    /// <summary>
+    /// Interaction logic for Register.xaml
+    /// </summary>
+    public partial class Register : Window
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void btnreg_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnlogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
